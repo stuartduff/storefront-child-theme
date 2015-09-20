94e2845d160dda5dc920ed39eb453fdf699e3424
Stuart Duff <stuartduff@users.noreply.github.com>
2015-09-20 21:53:04 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@
  * Loads the StoreFront parent theme stylesheet.
  */
 
-function storefront_child_theme_enqueue_styles() {
+function sf_child_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-add_action( 'wp_enqueue_scripts', 'storefront_child_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'sf_child_theme_enqueue_styles' );
 
 /**
  * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
